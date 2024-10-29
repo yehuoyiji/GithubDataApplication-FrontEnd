dc00c28224b9c4edee94144fbe2a8985e8d7851b
yehuo <1978002026@qq.com>
2024-10-29 16:19:13 +0800

    初始化根据github用户名搜索用户信息界面

---
+++ github-data-frontend/src/components/PersonalPage.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="personalPage">
+    <div class="container">
+      <div style="display: flex; flex: 1; justify-content: center;">
+        <a-input-search :loading="isSearchTrue"  @search="search" v-model="inputText"  size="large" :style="{width:'450px', borderRadius: '30px', maxHeight:'35px'}" placeholder="请输入github用户名" search-button>
+          <template #button-icon>
+            <icon-search/>
+          </template>
+          <template #button-default>
+            查找
+          </template>
+        </a-input-search>
+      </div>
+      <div style="display: flex; flex: 8;">
+        <div style="width: 100%; height: 100%; background-color: #f2f2f2; margin: 0 3vw; border-radius: 20px;">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import {IconSearch} from "@arco-design/web-vue/es/icon";
+import {ref} from "vue";
+const inputText = ref('')
+const isSearchTrue = ref(false)
+
+const search = () => {
+  isSearchTrue.value = true
+
+  setTimeout(() => {
+    inputText.value = ''
+    isSearchTrue.value = false
+  }, 1000);
+
+}
+</script>
+
+<style scoped>
+#personalPage {
+  width: 100%;
+  height: 100%;
+}
+.container{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex: 9;
+  flex-direction: column;
+}
+</style>
--- github-data-frontend/src/views/MainView.vue
+++ github-data-frontend/src/views/MainView.vue
@@ -1,13 +1,122 @@
 <template>
-  <div>
-    123q
+  <div id="main-view">
+    <div class="container">
+      <div class="container-header">
+        <div style="display: flex; align-items: center; justify-content: center; flex: 1">
+          <a-radio-group size="large" v-model="type" type="button" :style="{width: '200px', height: '40px'}">
+            <a-radio :style="{width: '50%', textAlign: 'center', lineHeight: '35px'}" value="个人">个人</a-radio>
+            <a-radio :style="{width: '50%', textAlign: 'center', lineHeight: '35px'}" value="局域">局域</a-radio>
+          </a-radio-group>
+        </div>
+      </div>
+      <div class="container-body">
+        <PersonalPage></PersonalPage>
+      </div>
+      <div class="container-footer">
+        <div style="display: flex; flex: 1; justify-content: center; align-items: center">
+          个人博客:https://yehuo.icu
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 
+import PersonalPage from "@/components/PersonalPage.vue";
+import {ref} from "vue";
+const type = ref('个人')
+
 </script>
 
 <style scoped>
+#main-view {
+  height: 100vh;
+  width: 100%;
+}
 
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.container-header {
+  display: flex;
+  flex: 1;
+}
+.container-body {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex: 9;
+  flex-direction: column;
+}
+.container-footer {
+  display: flex;
+  flex: 1;
+}
+.search-box {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 30px;
+  min-height: 50px;
+  height: 5vh;
+  width: 50vh;
+  min-width: 500px;
+  border: 1px solid #ffffff;
+  padding: 0 20px;
+  font-size: 1.2rem;
+  background-color: #f2f2f2;
+}
+
+.input-box {
+  display: flex;
+  flex: 8;
+  align-items: center;
+  width: 100%;
+}
+
+.search-button {
+  display: flex;
+  flex: 2;
+  min-width: 25%;
+  justify-content: center;
+  align-items: center;
+}
+
+input {
+  -webkit-box-shadow: 0 0 0 1000px #ffffff inset;
+}
+
+</style>
+<style>
+.arco-input-search  {
+  border-radius: 40px !important;
+}
+.arco-input-outer > :first-child {
+  //border-top-left-radius: var(--border-radius-small);
+  //border-bottom-left-radius: var(--border-radius-small);
+  border-top-left-radius: 15px;
+  border-bottom-left-radius: 15px;
+
+}
+.arco-input-search .arco-input-search-btn {
+  border-top-right-radius: 15px;
+  border-bottom-right-radius: 15px;
+}
+.arco-input-outer > :last-child {
+  //border-top-right-radius: var(--border-radius-small);
+  //border-bottom-right-radius: var(--border-radius-small);
+  border-top-right-radius: 15px;
+  border-bottom-right-radius: 15px;
+}
+.arco-radio-group-button {
+  border-radius: 10px;
+}
+.arco-radio-button {
+  border-radius: 10px;
+}
 </style>
